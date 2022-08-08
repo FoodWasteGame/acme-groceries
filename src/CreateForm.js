6a39b7339149b18738d93cb8975372d08b2186e6
Rdgh1583 <rommeldemano@gmail.com>
2022-08-07 23:24:42 -0400

    server

--- src/CreateForm.js
+++ src/CreateForm.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import { createGrocery } from './store';
 
-
-class CreateForm extends Component{
-  constructor(){
+class CreateForm extends Component {
+  constructor() {
     super();
     this.state = {
-      name: ''
+      name: '',
     };
   }
-  render(){
+  render() {
     const { name } = this.state;
     return (
       <form>
-        <input value={ name } onChange={ ev => this.setState({ name: ev.target.value})}/>
-        <button onClick={()=> this.props.create(this.state.name)}>Create</button>
+        <input
+          value={name}
+          onChange={(ev) => this.setState({ name: ev.target.value })}
+        />
+        <button onClick={() => this.props.create(this.state.name)}>
+          Create
+        </button>
       </form>
     );
   }
 }
 
-const mapDispatchToProps = (dispatch)=> {
+const mapDispatchToProps = (dispatch) => {
   return {
-    create: async(name)=> {
+    create: async (name) => {
       const grocery = (await axios.post('/api/groceries', { name })).data;
-      dispatch({ type: 'CREATE', grocery });
-    }
+      // dispatch({ type: 'CREATE', grocery });
+      dispatch(createGrocery(grocery));
+    },
   };
-}
+};
 
 export default connect(null, mapDispatchToProps)(CreateForm);
--- src/store.js
+++ src/store.js
@@ -1,25 +1,63 @@
 import { createStore } from 'redux';
 
+const LOAD = 'LOAD';
+const UPDATE = 'UPDATE';
+const CREATE = 'CREATE';
+const SET_VIEW = 'SET_VIEW';
+
 const initialState = {
   groceries: [],
-  view: ''
+  view: '',
 };
-const store = createStore((state = initialState, action)=> {
-  if(action.type === 'LOAD'){
-    state = {...state, groceries: action.groceries };
+const store = createStore((state = initialState, action) => {
+  if (action.type === LOAD) {
+    state = { ...state, groceries: action.groceries };
   }
-  if(action.type === 'UPDATE'){
-    state = {...state, groceries: state.groceries.map(grocery => grocery.id === action.grocery.id ? action.grocery : grocery )};
+  if (action.type === UPDATE) {
+    state = {
+      ...state,
+      groceries: state.groceries.map((grocery) =>
+        grocery.id === action.grocery.id ? action.grocery : grocery
+      ),
+    };
   }
-  if(action.type === 'CREATE'){
-    state = {...state, groceries: [...state.groceries, action.grocery ]}
+  if (action.type === CREATE) {
+    state = { ...state, groceries: [...state.groceries, action.grocery] };
   }
-  if(action.type === 'SET_VIEW'){
-    state = {...state, view: action.view};
+  if (action.type === SET_VIEW) {
+    state = { ...state, view: action.view };
   }
   return state;
 });
 
+const load = (groceries) => {
+  return {
+    type: 'LOAD',
+    groceries,
+  };
+};
+
+const update = (grocery) => {
+  return {
+    type: 'UPDATE',
+    grocery,
+  };
+};
+
+const createGrocery = (grocery) => {
+  return {
+    type: 'CREATE',
+    grocery,
+  };
+};
+
+const setView = (view) => {
+  return {
+    type: 'SET_VIEW',
+    view,
+  };
+};
+
 export default store;
 
-
+export { load, update, createGrocery, setView };
